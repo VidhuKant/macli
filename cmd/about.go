8ae5d24623fd425b2d5993c479c8eb3d48009941
Vidhu Kant Sharma <vidhukant@vidhukant.com>
2023-07-28 18:00:48 +0530

    removing seasonals

--- cmd/about.go
+++ cmd/about.go
@@ -1,6 +1,6 @@
 /*
 macli - Unofficial CLI-Based MyAnimeList Client
-Copyright © 2022 Vidhu Kant Sharma <vidhukant@vidhukant.xyz>
+Copyright © 2022 Vidhu Kant Sharma <vidhukant@vidhukant.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -30,7 +30,7 @@ var aboutCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("macli is an unofficial MyAnimeClient for use inside the terminal.\n" +
 			"\n" +
-			"\x1b[34mmacli  Copyright (C) 2022  Vidhu Kant Sharma <vidhukant@vidhukant.xyz>\n" +
+			"\x1b[34mmacli  Copyright (C) 2022  Vidhu Kant Sharma <vidhukant@vidhukant.com>\n" +
 			"This program comes with ABSOLUTELY NO WARRANTY;\n" +
 			"This is free software, and you are welcome to redistribute it\n" +
 			"under certain conditions; For details refer to the GNU General Public License.\n" +
@@ -38,7 +38,7 @@ var aboutCmd = &cobra.Command{
 			"along with this program.  If not, see <https://www.gnu.org/licenses/>.\x1b[0m\n" +
 			"\n" +
 			"\x1b[35mPlease report any bugs on the GitHub page: https://github.com/MikunoNaka/macli\n" +
-			"or through email: vidhukant@vidhukant.xyz\x1b[0m\n\n",
+			"or through email: vidhukant@vidhukant.com\x1b[0m\n\n",
 		)
 		fmt.Println("Visit \x1b[36mhttps://www.vidhukant.xyz/docs/macli/\x1b[0m for more info.")
 		fmt.Println("Run \x1b[33m`macli --help`\x1b[0m for instructions.")
